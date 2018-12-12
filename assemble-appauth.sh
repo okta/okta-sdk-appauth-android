4602799b18d4ce057b4c78fdc084553ff2202503
Sergiy Mokiyenko <smokiyenko@lohika.com>
2018-12-12 15:54:57 +0200

    adds travis changes for appauth build support

--- assemble-appauth.sh
+++ assemble-appauth.sh
@@ -2,7 +2,7 @@
 
 cd $1
 
-if [[ ! -f $2libs/appauth-release.aar ]]; then
+if [ ! -f $2libs/appauth-release.aar ]; then
     ./gradlew assembleRelease
     mkdir $2/libs
     cp $1/library/build/outputs/aar/appauth-release.aar $2/libs/appauth-release.aar
